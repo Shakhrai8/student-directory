d3cec4de319d3ea0db6aaab6eca305c41a261a75
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-04-27 19:24:31 +0100

    sorted all puts commands into methods

--- directory.rb
+++ directory.rb
@@ -12,11 +12,23 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-#add then print them
-puts "The students of Villains Academy"
-puts "--------------------------------"
-students.each do |student|
-  puts student
+#method for printing the header
+def print_header
+  puts "The students of Villains Academy"
+  puts "--------------------------------"
 end
-#finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+#method for printing each student name from the array
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+#method for printing total number of students
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
