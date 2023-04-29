7aab4e77c5e02eceb456b80b6f88c39f69ae8571
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-04-29 16:35:20 +0100

    Refactored the code for interactive menu

--- directory.rb
+++ directory.rb
@@ -1,21 +1,19 @@
+#create an empty array
+@students = []
 #method for getting list of names from the user input into array
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create an empty array
-  students = []
   #get the first name
   name = gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     #get another name from the user
     name = gets.chomp
   end
-  #return the array of students
-  students
 end
 #method for printing the header
 def print_header
@@ -23,40 +21,46 @@ def print_header
   puts "--------------------------------"
 end
 #method for printing each student name from the array
-def print(names)
-  names.each.with_index(1) do |name, index|
+def print_students_list
+  @students.each.with_index(1) do |name, index|
     puts "#{index}.#{name[:name]} (#{name[:cohort]} cohort)"
   end
 end
 #method for printing total number of students
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
-#final method for our interactive menu which will call other methods 
-def interactive_menu
-  #Declared variable student again so it will be available in several iterations of the loop
-  students = []
-  loop do
-    # print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    #read the input and save it into a variable
-    selection = gets.chomp
-    #do what the has asked
-    case selection
+#method for printing the menu options
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+#method for printing student list
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+#method for selection
+def process(selection)
+  case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit #this will cause the program to terminate
     else
       puts "I don't know what you meant, try again"
-    end
   end
+end
+#final method for our interactive menu which will call other methods 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end  
 end
 #nothing gonna happen until we call our method for the menu
 interactive_menu
