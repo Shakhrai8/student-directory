fc9e09bb46f860d7c3ba866be97822b8a4026133
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-04-29 17:03:03 +0100

    Added new method for loading files

--- directory.rb
+++ directory.rb
@@ -35,6 +35,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 #method for printing student list
@@ -55,6 +56,15 @@ def save_students
   end
   file.close
 end
+#method for loading files
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
 #method for selection
 def process(selection)
   case selection
@@ -64,6 +74,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit #this will cause the program to terminate
     else
