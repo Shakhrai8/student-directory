5963928625ef5b373e30dd5b9fe58de4f43861bf
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-04-29 17:26:04 +0100

    Added new method for preloading file with a filename as an argument

--- directory.rb
+++ directory.rb
@@ -5,14 +5,14 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   #get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     #get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 #method for printing the header
@@ -57,13 +57,25 @@ def save_students
   file.close
 end
 #method for loading files
-def load_students
+def load_students(filename = "students.csv")
   file = File.open("students.csv", "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
+end
+#method for loading the program with argument
+def try_load_students
+  filename = ARGV.first #first argument from the command line
+  return if filename.nil? #get out of the method if it isn't given
+  if File.exist?(filename) #if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else #if it doesnt exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit #quit the program
+  end
 end
 #method for selection
 def process(selection)
@@ -86,8 +98,9 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end  
 end
 #nothing gonna happen until we call our method for the menu
+try_load_students
 interactive_menu
