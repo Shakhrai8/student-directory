4c09fe7149f384c0678f4375b3290d0eb4b6b8c8
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-05-01 14:06:10 +0100

    Program which should print itself

---
+++ selfprint.rb
@@ -0,0 +1,22 @@
+#quine program
+lines = [
+  "lines = [",
+  "__PRINT_OWN_SOURCE_CODE__",
+  "]",
+  "",
+  "lines.each. do |line|",
+  "  if line == '__PRINT_OWN_SOURCE_CODE__'",
+  "    lines .each {|l| puts ' ' << l.inspect << ',' }",
+  "  else",
+  "    puts line",
+  "  end",
+  "end",
+]
+
+lines.each do |line|
+  if line == '__PRINT_OWN_CODE__'
+    lines.each {|l| puts ' ' << l.inspect << ',' }
+  else
+    puts line
+  end
+end
